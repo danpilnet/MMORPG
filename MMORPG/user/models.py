f9c278ebe72f20413e5deaf8947935bfcca55b73
danpilnet <dnpdnp28@gmail.com>
2024-09-11 18:03:19 +0900

    model user

--- MMORPG/user/models.py
+++ MMORPG/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    groups = None
+    user_permissions = None
+    image = models.ImageField(null=True, blank=True, verbose_name='Image')
